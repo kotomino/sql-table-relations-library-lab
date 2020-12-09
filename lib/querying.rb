a7818bdfb6ec37470d8bce045603593caf655f10
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-12-08 19:39:41 -0500

    commit tree 800d188c984a794a49a2adc646c28a45fabc6daf

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,66 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+    SELECT title, year
+    FROM Books
+    WHERE series_id = 1
+    ORDER BY year
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+    SELECT name, motto
+    FROM Characters
+    ORDER BY LENGTH(motto) DESC LIMIT 1;
+  SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+    SELECT species, COUNT(species)
+    FROM Characters
+    GROUP BY species 
+    ORDER BY COUNT(species) DESC LIMIT 1;
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Authors.name, Subgenres.name
+    FROM Authors
+    JOIN Series
+    ON Authors.id = Series.author_id
+    JOIN Subgenres
+    ON Subgenres.id = Series.subgenre_id
+  SQL
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Series.title
+    FROM Series
+    JOIN Books
+    ON Series.id = Books.series_id
+    JOIN Character_books
+    ON Books.id = Character_books.book_id
+    JOIN Characters
+    ON Characters.id = Character_books.character_id
+    WHERE Characters.species = "human"
+    GROUP BY Series.title
+    ORDER BY COUNT(Characters.species) DESC LIMIT 1;
+
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  <<-SQL
+    SELECT Characters.name, COUNT(*)
+    FROM Characters
+    JOIN Character_books
+    ON Characters.id = Character_books.character_id
+    GROUP BY Characters.name
+    ORDER BY COUNT(*) DESC, Characters.name;
+  SQL
+
 end
